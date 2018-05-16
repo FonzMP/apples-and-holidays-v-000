1bc5aa3f6cb75eb1eb741b76cd46635245588687
Alex Wilson <fonzmp@gmail.com>
2018-05-16 04:10:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holder = []
+  holiday_hash.each do |key, values|
 end
 
 def all_holidays_with_bbq(holiday_hash)
