40340a1ee3cdc3dbc36a50f6d5438b7e5baedbe6
Alex Wilson <fonzmp@gmail.com>
2018-05-16 04:07:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   holder = []
   holiday_hash.each do |key, value|
     value.each do |attribute, data|
-      attribute.each do |item|
+      attribute.each do |item, list|
         holder << item
       end
     end
