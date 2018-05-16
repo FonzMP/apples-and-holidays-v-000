86a6e9deeb5e49b91af59274f612047a81300648
Alex Wilson <fonzmp@gmail.com>
2018-05-16 03:55:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |key, value|
     if key == :winter
       value.each do |attribute, data|
-        attribute[data] << supply
+        attribute[data].push(supply) supply
       end
     end
   end
