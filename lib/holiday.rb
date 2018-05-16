1539e9642dfdd2b74c2cd71277dac88ee47a2426
Alex Wilson <fonzmp@gmail.com>
2018-05-16 04:24:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   string = ""
   holiday_hash.each do |key, value|
-    string += "#{key.capitalize}:" "\n"
+    string += "#{key.capitalize}:"
+    string += "\n"
     value.each do |season, holiday|
       string += "  #{season.capitalize}: #{holiday.join(", ")}"
     end
